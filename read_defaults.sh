aa4412b4b9a71f917e726fec162345adfad7cc2e
s2221190533 <27883767-s2221190533@users.noreply.replit.com>
2026-05-18 12:52:08 +0000

    Git commit prior to merge

--- read_defaults.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BUNDLE_ID="com.yourname.MedAi.MEDSAI-"
-APP_CONTAINER=$(xcrun simctl get_app_container booted "$BUNDLE_ID" data)
-PREF="$APP_CONTAINER/Library/Preferences/$BUNDLE_ID"
-
-echo "Reading deviceToken:"
-defaults read "$PREF" deviceToken
--- scripts/post-merge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Post-merge setup complete (no build steps required for this project)."
